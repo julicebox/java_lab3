ae7f4549731d527eb65d58d768a9e1ac2d7c62b4
julicebox <gurskaya.yuliya.04@gmail.com>
2022-12-18 23:34:40 +0300

    Initial commit

---
+++ src/bsu/rfct/course2/group7/Gurskaya/Main.java
@@ -0,0 +1,14 @@
+package bsu.rfct.course2.group7.Gurskaya;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Double[] a = {1., 5., 4., 3., 2., 1.};
+
+        HornersScheme frame = new HornersScheme(a);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
